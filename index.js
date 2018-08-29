005fbff6ff30be9f7e220b31fcdba840c4a8ae64
David Rafe <david.rafe@gmail.com>
2018-08-29 21:51:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
-function happyHolidaysTo(name, holiday) {
+function happyHolidayTo(name, holiday) {
   return `Happy ${holiday}, ${name}!`
 }
 function holidayCountdown(holiday, days) {
